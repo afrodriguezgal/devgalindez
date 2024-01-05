1b8f3f3dfdf046e611f94c4b466a54738967b357
developer binario <andres2412.st@gmail.com>
2024-01-05 13:53:14 -0500

    eliminacion de archivos de prueba

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const typewriteElement = document.getElementById('typewrite');
+const text = 'aqui tu presentación';
+
+function typeWrite(text, i, fncallback){
+    if (i < text.length){
+        typewriteElement.innerHTML = text.subtring(0,i + 1) + '<span aria-hidden="true"></span>';
+        setTimeout(function(){
+            typeWrite(text, i + 1, fncallback)
+        }, 100);
+    }
+        else if (typeof fncallback== 'function'){
+            setTimeout(fncallback, 700);
+        }
+    }
+typeWrite(text, 0, function(){
+    
+})
